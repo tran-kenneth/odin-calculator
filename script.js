39db8fac28a15253ee0362bb77d0d4a1e5741db9
Kenneth Tran <trank.dev@gmail.com>
2022-06-22 22:14:43 -0400

    Implement functionality to update display

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-function add (num1, num2){
-    return num1 + num2;
+let display = 0;
+
+function updateDisplay() {
+  const displayElement = document.getElementsByClassName("calc-display")[0];
+  displayElement.textContent = display;
 }
 
-function subtract (num1, num2){
-    return num1 - num2;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function multiple (num1, num2){
-    return num1 * num2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function divide (num1, num2){
-    return num1 / num2;
+function multiple(num1, num2) {
+  return num1 * num2;
 }
 
-function operate (operator, num1, num2){
-    return operator(num1, num2);
+function divide(num1, num2) {
+  return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+  return operator(num1, num2);
+}
+
+updateDisplay();
