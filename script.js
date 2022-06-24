8e0f615f3ac7388ac7f655f309b4468b89ec65ae
Kenneth Tran <trank.dev@gmail.com>
2022-06-24 14:44:20 -0400

    Add values and event listeners to buttons

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ function updateDisplay() {
   displayElement.textContent = display;
 }
 
+function addEventListenersToButtons() {
+  const buttons = document.querySelectorAll("button");
+  console.log(buttons);
+  console.log(buttons[0].classList.contains("operand"));
+
+  buttons.forEach((button) => {
+    if (button.classList.contains("operand")) {
+      button.addEventListener("click", logEventValue);
+    } else if (button.classList.contains("operator")) {
+      button.addEventListener("click", logEventValue);
+    } else if (button.classList.contains("sign")) {
+      button.addEventListener("click", logEventValue);
+    } else if (button.classList.contains("decimal")) {
+      button.addEventListener("click", logEventValue);
+    } else if (button.classList.contains("equals")) {
+      button.addEventListener("click", logEventValue);
+    } else if (button.classList.contains("clear")) {
+      button.addEventListener("click", logEventValue);
+    }
+  });
+}
+
+function logEventValue(e) {
+  console.log(e.srcElement.value);
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -26,3 +52,4 @@ function operate(operator, num1, num2) {
 }
 
 updateDisplay();
+addEventListenersToButtons();
